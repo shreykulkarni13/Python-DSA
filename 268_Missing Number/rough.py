32b16454041cbab446c0d78ace971aefcbf253cd
shreykulkarni13 <shreykulkarni13@gmail.com>
2026-07-13 05:47:40 +0530

    Problem 268_Missing Number

---
+++ 268_Missing Number/rough.py
@@ -0,0 +1,15 @@
+nums = [9,6,4,2,3,5,7,0,1]
+
+
+def missingNumber(nums):
+    n = len(nums)
+
+    for i in range (0,(n+1)):
+        if (i in nums ):
+            continue
+        
+        else:
+            return i
+        
+print(missingNumber(nums))
+        
